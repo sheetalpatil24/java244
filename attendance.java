ec694b12554209f3452133237dd64b8e01a74d87
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-05 12:49:37 +0530

    hud

---
+++ attendance.java
@@ -0,0 +1,14 @@
+public class EmpWageattendancce {
+
+    public static void main (String[]args) {
+        //constant
+        int IS_FULL_TIME =1;
+        //COMPUTATION
+        double empCheck = Math.floor(math.random()*10) %2;
+        if (empCheck == IS_FULL_TIME)
+          System.out.println (Employee is Present");
+          else
+          System.out.println(Employee is abscent");
+    }
+}
+EmpWageattendancce.java (END)
